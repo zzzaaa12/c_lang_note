09aa6a518c0e1b292bf6dcc60b9f2f5ebe1434de
Alex_Lin <Alex_Lin@cameo.com.tw>
2016-01-22 11:05:53 +0800

    change strncat() to strncpy()

--- string/string.c
+++ string/string.c
@@ -10,7 +10,7 @@ char *str_replace(char *str, const char *old, const char *new)
 {
 	int len, len_old, len_new;
 	int count = 0;
-	char *ptr, *next, *new_str;
+	char *pos, *ptr, *next, *new_str;
 
 	if (str == NULL || old == NULL || new == NULL)
 		return NULL;
@@ -19,6 +19,7 @@ char *str_replace(char *str, const char *old, const char *new)
 	len_old = strlen(old);
 	len_new = strlen(new);
 
+	/* count old string */
 	ptr = str;
 	while (ptr) {
 		ptr = strstr(ptr, old);
@@ -28,24 +29,37 @@ char *str_replace(char *str, const char *old, const char *new)
 		ptr++;
 	}
 
+	/* allocate enough space to store the result string */
 	new_str = malloc(len + (len_new - len_old) * count + 1);
 	if (new_str == NULL)
 		return NULL;
 
 	memset(new_str, 0, len + (len_new - len_old) * count + 1);
+	pos = new_str;
+
+	/* copy the first part before first old string */
 	ptr = strstr(str, old);
-	if (ptr != str)
-		strncat(new_str, str, ptr - str);
+	if (ptr && ptr != str) {
+		strncpy(new_str, str, ptr - str);
+		pos = new_str + (ptr - str);
+	}
 
 	while (ptr) {
-		strncat(new_str, new, len_new); // copy new string
+
+		/* copy new string to replace old string */
+		strncpy(pos, new, len_new); // copy new string
+		pos = pos + len_new;
 		next = strstr(ptr + 1, old);
-		if (next == NULL) { // copy last old string
-			strcat(new_str, ptr + len_old);
+
+		/* copy last string */
+		if (next == NULL) {
+			strcpy(pos, ptr + len_old);
 			break;
 		}
 
-		strncat(new_str, ptr + len_old, next - (ptr + len_old));
+		/* copy other string */
+		strncpy(pos, ptr + len_old, next - (ptr + len_old));
+		pos += next - (ptr + len_old);
 		ptr = next;
 	}
 
@@ -95,14 +109,14 @@ int strstr_count(const char *str, const char *find)
 
 int main(void)
 {
-	char aa[] = "xxxxx11xxxxx11aaaaa112";
+	char aa[] = "11xxxxx11xxxxx11aaaaa11211";
 	char *test;
 
 	printf("%d\n", strstr_count("123456123456312313143425346123456", "123"));
 	printf("%d\n", strchr_count("aaa12345612345a6312a31314a34253461a23456a", 'a'));
 
 	printf("old=%s\n", aa);
-	test = str_replace(aa, "11", "!!!!!!!!!!!!!!!");
+	test = str_replace(aa, "11", "!!!!!!!!");
 	if (test) {
 		printf("new=%s\n", test);
 		free(test);
