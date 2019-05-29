a8680ed67f5c1500fdb1ae5619b8af958fdaa841
zzzaaa12 <zzzaaa12@users.noreply.github.com>
2019-05-29 13:52:39 +0800

    Update cprintf.c

--- debug/cprintf.c
+++ debug/cprintf.c
@@ -5,7 +5,7 @@ FILE *fp_console;
 do { \
     fp_console = fopen("/dev/console","w"); \
     if (fp_console) { \
-        fprintf(fp_console, "[%s %d]: " msg "\n", __func__,__LINE__,##__VA_ARGS__); \
+        fprintf(fp_console, "[%s %d]: " msg "\n", __func__, __LINE__, ##__VA_ARGS__); \
         fclose(fp_console); \
     }; \
 } while(0)
