1f52bc4cedcd1970470a0f4c93fbf83e23dc609b
zzzaaa12 <zzzaaa12@users.noreply.github.com>
2019-05-29 12:08:14 +0800

    Create cprintf.c

---
+++ debug/cprintf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+FILE *fp_console;
+#define cprintf(msg, ...) \
+do { \
+    fp_console = fopen("/dev/console","w"); \
+    if (fp_console) { \
+        fprintf(fp_console, "[%s %d]: " msg "\n", __func__,__LINE__,##__VA_ARGS__); \
+        fclose(fp_console); \
+    }; \
+} while(0)
